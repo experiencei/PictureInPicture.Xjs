6d8ffb672534231d90cfe26cb0f7f33d89ac64a0
experiencei <ayelojahighbee01@gmail.com>
2021-08-10 18:36:47 +0100

    Changes made on script.js to enhance perfomance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const videoElement = document.getElementById('video');
-const button = document.getElementById('button');
+const videoElement = document.getElementById('video'),
+       button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
